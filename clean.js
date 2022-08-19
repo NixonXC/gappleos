0d68aeea7ed049c94d16283e91d60763a8d33226
NixonXC <invasionyt19@gmail.com>
2022-08-19 14:09:28 +0530

    Add files via upload

---
+++ clean.js
@@ -0,0 +1,6 @@
+document.getElementById("home").onclick = function() {
+    window.location.href = "index.html";
+}
+document.getElementById("arrow").onclick = function() {
+    window.location.href = "index.html";
+}
---
+++ search.js
@@ -0,0 +1,9 @@
+document.getElementById("home").onclick = function() {
+    window.alert("You're Already Here!");
+}
+document.getElementById("arrow").onclick = function() {
+    window.alert("You're Already Here!");
+}
+document.getElementById("drive").onclick = function() {
+    window.location.href = "clean.html";
+}
